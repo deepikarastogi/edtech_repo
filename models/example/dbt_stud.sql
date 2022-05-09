a025fcde61b1ddc001dd380554dbaed2dc67db0e
goeld7898@gmail.com <goeld7898@gmail.com>
2022-05-09 13:01:46 +0000

    student_table

---
+++ models/example/dbt_stud.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='view') }}
+
+select student_id,
+user_profile,
+marks,
+tenants as ten1,
+total_assignments,
+assignments_attempted,
+assignments_left,
+student_enrollment_year,
+time_spent,
+countries,
+first_login,
+last_activity_since,
+days_difference,
+user_category from edtech.app.students_final
+
